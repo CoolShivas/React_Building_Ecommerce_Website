8e842f52935abd32ced0c4564c8400c26d72cb39
CoolShivas <shivas2710cool00@gmail.com>
2024-04-11 15:27:10 +0530

    Implementation of Ecommerce Website

---
+++ Building_Ecommerce_Website/src/App.jsx
@@ -0,0 +1,14 @@
+import AppName from "./components/AppName";
+import "./App.module.css";
+
+function App(){
+  return <>
+  <header>
+    <AppName></AppName>
+  </header>
+  <main></main>
+  <footer></footer>
+  </>
+}
+
+export default App;
---
+++ Building_Ecommerce_Website/src/components/AppName.jsx
@@ -0,0 +1,10 @@
+import classes from "./AppName.module.css";
+
+
+const AppName = () =>{
+    return <center>
+        <h1 className={classes.generic}> The Generics </h1>
+    </center>
+}
+
+export default AppName;
