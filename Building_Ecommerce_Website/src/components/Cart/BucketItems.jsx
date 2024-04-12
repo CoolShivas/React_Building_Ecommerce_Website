779ecfc2e979cfcdd790f902e4e3bcfa95edc848
CoolShivas <shivas2710cool00@gmail.com>
2024-04-12 16:08:10 +0530

    BucketItems_passing_in_table_format

--- Building_Ecommerce_Website/src/components/Cart/BucketItems.jsx
+++ Building_Ecommerce_Website/src/components/Cart/BucketItems.jsx
@@ -1,54 +1,79 @@
 import classes from "./BucketItems.module.css";
-import ModalOverlay from "../../UI/ModalOverlay"
+import ModalOverlay from "../../UI/ModalOverlay";
 
+const BucketItems = (props) => {
+  const cartElements = [
+    {
+      title: "Colors",
 
+      price: 100,
 
-const BucketItems = (props) =>{
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
 
-    const cartElements = [
+      quantity: 2,
+    },
 
-        {
-        
-        title: 'Colors',
-        
-        price: 100,
-        
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-        
-        quantity: 2,
-        
-        },
-        
-        {
-        
-        title: 'Black and white Colors',
-        
-        price: 50,
-        
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-        
-        quantity: 3,
-        
-        },
-        
-        {
-        
-        title: 'Yellow and Black Colors',
-        
-        price: 70,
-        
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-        
-        quantity: 1,
-        
-        }
-        
-        ];
+    {
+      title: "Black and white Colors",
 
-        
+      price: 50,
 
-    return <ModalOverlay hideBucketABC={props.hideBucketABC}>
-    <ul className={classes.second_ul__tag}>
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+
+      quantity: 3,
+    },
+
+    {
+      title: "Yellow and Black Colors",
+
+      price: 70,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+
+      quantity: 1,
+    },
+  ];
+
+  return (
+    <ModalOverlay hideBucketABC={props.hideBucketABC}>
+      <table className="table">
+        <thead>
+          <tr>
+            <th> Poduct </th>
+            <th> Price </th>
+            <th> Quantity </th>
+            <th> Total </th>
+            <th> Button </th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartElements.map((brr) => {
+            return (
+              <tr key={brr.title}>
+                <th> {brr.title} </th>
+                <th>
+                  {" "}
+                  <img
+                    src={brr.imageUrl}
+                    alt="cart items image not found"
+                  />{" "}
+                </th>
+                <th> Rs.{brr.price}/- </th>
+                <th> {brr.quantity} </th>
+                <th>
+                  {" "}
+                  <button className={classes.btn_remove}> Remove </button>{" "}
+                </th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {/* <ul className={classes.second_ul__tag}>
         {cartElements.map((brr)=>{
             return <li key={brr.title}>
                 Title: {brr.title}
@@ -56,9 +81,11 @@ const BucketItems = (props) =>{
               <button className={classes.btn_remove}> Remove </button>
             </li>
         })}
-    </ul>
-    <button onClick={props.hideBucketABC}> Close Bucket </button>
+    </ul> */}
+
+      <button onClick={props.hideBucketABC}> Close Bucket </button>
     </ModalOverlay>
-}
+  );
+};
 
 export default BucketItems;
--- Building_Ecommerce_Website/src/components/Data/GettingData.jsx
+++ Building_Ecommerce_Website/src/components/Data/GettingData.jsx
@@ -6,50 +6,7 @@ import CartContext from "../../store/CartContext";
 const GettingData = () =>{
 
    const {itemsAvailable} = useContext(CartContext);
-    // const productsArr = [
-
-    //     {
-        
-    //     title: 'Colors',
-        
-    //     price: 100,
-        
-    //     imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-        
-    //     },
-        
-    //     {
-        
-    //     title: 'Black and white Colors',
-        
-    //     price: 50,
-        
-    //     imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-        
-    //     },
-        
-    //     {
-        
-    //     title: 'Yellow and Black Colors',
-        
-    //     price: 70,
-        
-    //     imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-        
-    //     },
-        
-    //     {
-        
-    //     title: 'Blue Color',
-        
-    //     price: 100,
-        
-    //     imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
-        
-    //     }
-        
-    //     ];
-        
+   
     return <div className={classes.unorder_list}>
     <ul>
         {itemsAvailable.map((arr)=>{
