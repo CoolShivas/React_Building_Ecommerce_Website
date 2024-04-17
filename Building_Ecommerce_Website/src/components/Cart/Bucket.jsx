eda0579976d1a6ffa25c7033b6d9f5e179e30bc5
CoolShivas <shivas2710cool00@gmail.com>
2024-04-17 12:45:46 +0530

    Displaying_Badge_Qty_on_Cart_Icon

--- Building_Ecommerce_Website/src/components/Cart/Bucket.jsx
+++ Building_Ecommerce_Website/src/components/Cart/Bucket.jsx
@@ -1,14 +1,24 @@
 import CartIcon from "./CartIcon";
 import classes from "./Bucket.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/CartContext";
 
 const Bucket = ({ showBucketXYZ }) => {
+  const { cartItems } = useContext(CartContext);
+
+  let qty = 0;
+  cartItems.forEach((arr) => {
+    return (qty = qty + Number(arr.quantity));
+  });
+
   return (
     <div className={classes.cart_with_badge}>
       <button className={classes.btn_cart} onClick={showBucketXYZ}>
         <CartIcon></CartIcon>
         <span className={classes.cart_only}> Cart </span>
       </button>
-      <span className={classes.badge}> 0 </span>
+      {/* <span className={classes.badge}> 0 </span> */}
+      <span className={classes.badge}> {qty} </span>
     </div>
   );
 };
