9645cebd0db2465a89452ad517d3d63f52846bd2
CoolShivas <shivas2710cool00@gmail.com>
2024-04-18 22:21:12 +0530

    Implement_HomePage_with_Data_and_CSS

--- Building_Ecommerce_Website/src/MenuNavbar/HomePage.jsx
+++ Building_Ecommerce_Website/src/MenuNavbar/HomePage.jsx
@@ -1,13 +1,72 @@
+import classes from "./HomePage.module.css";
+
 const HomePage = () => {
   return (
-    <>
-      <h2> Welcome to the HomePage</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error officia
-        vero tenetur dolor eius aspernatur, vitae dolores! Repudiandae, sit
-        suscipit.
-      </p>
-    </>
+    <div className={classes.homepage_container}>
+      {/* <h2> Welcome to the HomePage</h2> */}
+      <h2 className={classes.tour_heading}> Tours </h2>
+      <table className="table">
+        <tbody>
+          {/* first row */}
+          <tr>
+            <th> July 16 </th>
+            <th>DETROIT, MI</th>
+            <th>DTE ENERGY MUSIC THEATRE</th>
+            <th>
+              <button className={classes.btn}> buy tickets </button>
+            </th>
+          </tr>
+
+          {/* second row */}
+          <tr>
+            <th> July 19 </th>
+            <th>TORONTO,ON</th>
+            <th>BUDWEISER STAGE</th>
+            <th>
+              <button className={classes.btn}> buy tickets </button>
+            </th>
+          </tr>
+          {/* third row */}
+          <tr>
+            <th> July 22 </th>
+            <th>BRISTOW, VA</th>
+            <th>JIGGY LUBE LIVE</th>
+            <th>
+              <button className={classes.btn}> buy tickets </button>
+            </th>
+          </tr>
+
+          {/* fourth row */}
+          <tr>
+            <th> July 29 </th>
+            <th>PHOENIX, AZ</th>
+            <th>AK-CHIN PAVILION</th>
+            <th>
+              <button className={classes.btn}> buy tickets </button>
+            </th>
+          </tr>
+
+          {/* fifth row */}
+          <tr>
+            <th> AUG 2 </th>
+            <th>LAS VEGAS, NV</th>
+            <th>T-MOBILE ARENA</th>
+            <th>
+              <button className={classes.btn}> buy tickets </button>
+            </th>
+          </tr>
+          {/* sixth row */}
+          <tr>
+            <th> AUG 2 </th>
+            <th>CONCORD, CA</th>
+            <th>CONCORD PAVILION</th>
+            <th>
+              <button className={classes.btn}> buy tickets </button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- Building_Ecommerce_Website/src/components/AppName.jsx
+++ Building_Ecommerce_Website/src/components/AppName.jsx
@@ -1,10 +1,18 @@
+import { CgPlayButtonO } from "react-icons/cg";
 import classes from "./AppName.module.css";
 
-
-const AppName = () =>{
-    return <center>
-        <h1 className={classes.generic}> The Generics </h1>
+const AppName = () => {
+  return (
+    <center>
+      <div className={classes.generic}>
+        <h1> The Generics </h1>
+        <h4> Get our latest Album </h4>
+        <span className={classes.appName_span}>
+          <CgPlayButtonO />
+        </span>
+      </div>
     </center>
-}
+  );
+};
 
 export default AppName;
