d6982fcb8d17b58ab04c4b3400432b5f01319946
CoolShivas <shivas2710cool00@gmail.com>
2024-04-18 11:39:58 +0530

    Resolve_thePage_re-refresh_problem_using_NavLink_while_clickingOn_HomeIcons_andAll

--- Building_Ecommerce_Website/src/components/Header/Navbar.jsx
+++ Building_Ecommerce_Website/src/components/Header/Navbar.jsx
@@ -1,10 +1,11 @@
+import { NavLink } from "react-router-dom";
 import Bucket from "../Cart/Bucket";
 import classes from "./Navbar.module.css";
 
 const Navbar = ({ showBucketABC }) => {
   return (
     <div className={classes.navbar}>
-      <a className={classes.anchor} href="/">
+      {/* <a className={classes.anchor} href="/">
         home
       </a>
       <a className={classes.anchor} href="/store">
@@ -12,7 +13,18 @@ const Navbar = ({ showBucketABC }) => {
       </a>
       <a className={classes.anchor} href="/about">
         about
-      </a>
+      </a> */}
+
+      <NavLink to="/" className={classes.anchor}>
+        home
+      </NavLink>
+      <NavLink to="/store" className={classes.anchor}>
+        store
+      </NavLink>
+      <NavLink to="/about" className={classes.anchor}>
+        about
+      </NavLink>
+
       <Bucket showBucketXYZ={showBucketABC}></Bucket>
     </div>
   );
