78fa13b7bee0cc524f51e61a613654a6236a8567
CoolShivas <shivas2710cool00@gmail.com>
2024-04-27 12:29:59 +0530

    Adding_ProductPage_and_images

--- Building_Ecommerce_Website/src/MenuNavbar/ProductPage.jsx
+++ Building_Ecommerce_Website/src/MenuNavbar/ProductPage.jsx
@@ -1,21 +1,41 @@
-
+import classes from "./ProductPage.module.css";
 
 const ProductPage = () => {
   return (
-    <div className="product_page__div">
-        <div className="grid_two__columns">
-            <div className="product_images">
-                <p> Image One </p>
-                <p> Image One </p>
-                <p> Image One </p>
-            </div>
-            <div className="product_reviews">
-            <p> Good </p>
-            <p> Bad </p>
-            </div>
+    <div className={classes.product_page__div}>
+      <div className={classes.grid_three__columns}>
+        <div className={classes.grid_two__columns}>
+          <div className={classes.product_images}>
+            <img
+              src="../../public/images/Ear_Pods.webp"
+              alt="image not found"
+            />
+            <img src="../../public/images/Ipod.webp" alt="image not found" />
+            <img
+              src="../../public/images/Pocket_Min_Printer.webp"
+              alt="image not found"
+            />
+            <img
+              src="../../public/images/SmartPhone.avif"
+              alt="image not found"
+            />
+          </div>
+          <div className={classes.product_single__image}>
+            <img
+              src="../../public/images/Pocket_Min_Printer.webp"
+              alt="image not found"
+            />
+          </div>
         </div>
+        <div className="product_reviews">
+          <p> Good </p>
+          <p> Bad </p>
+          <p> Fantastic </p>
+          <p> Marvolous </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductPage;
