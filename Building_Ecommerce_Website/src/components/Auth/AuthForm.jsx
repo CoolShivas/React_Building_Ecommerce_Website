f22c16f9c954bde25d3d75b7e798f99a7893d9c8
CoolShivas <shivas2710cool00@gmail.com>
2024-05-02 10:42:38 +0530

    Declare useState for LogIn and SignUp with SwitchAuthMode Toggler with Conditions

--- Building_Ecommerce_Website/src/components/Auth/AuthForm.jsx
+++ Building_Ecommerce_Website/src/components/Auth/AuthForm.jsx
@@ -1,34 +1,53 @@
+import axios from "axios";
+import { useRef, useState } from "react";
 import classes from "./AuthForm.module.css";
 
-
 const AuthForm = () => {
+  const [isLogin, setIsLogIn] = useState(true);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogIn(prevState => !prevState);
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <section className={classes.auth}>
-    {/* <h1> Login / SignUp </h1> */}
-    <h1> Login </h1>
+      <h1> {isLogin ? "Login" : "SignUp"} </h1>
 
-    <form>
-      <div className={classes.control}>
-        <label htmlFor="email">Your Email</label>
-        <input type="email" id="email" required/>
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="password">Your Password</label>
-        <input type="password" id="password" required />
-      </div>
-      <div className={classes.actions}>
-         <button> Login </button>
-        
-        <button
-          type="button"
-          className={classes.toggle}
-        >
-          SignUp
-        </button>
-      </div>
-    </form>
-  </section>
-  )
-}
+      <form onSubmit={submitFormHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          <button> {isLogin ? "Login" : "Create Account"} </button>
+
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default AuthForm;
